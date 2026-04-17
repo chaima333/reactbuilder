4ff5c66b952e11311f295128dc167c139701c72b
Chaima Kaboudi <kaboudichaima123@gmail.com>
2026-04-17 16:51:20 +0200

    Fix page model indexing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -44,9 +44,9 @@ app.use(authenticateJWT);
 //app.use(tenantResolver); 
 
 // --- 4. ROUTES PROTÉGÉES (تخدم بالـ Token والـ Role) ---
-app.use("/api/pages", pageRoutes);
-app.use("/api/sites", siteRoutes);
-app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/pages",tenantResolver, pageRoutes);
+app.use("/api/sites",tenantResolver, siteRoutes);
+app.use("/api/dashboard",tenantResolver, dashboardRoutes);
 app.use("/api/media", mediaRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/seo", seoRoutes);
