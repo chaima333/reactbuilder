ac78ce6b470bd0ef98bd5c2644b7cf3851862d52
Chaima Kaboudi <kaboudichaima123@gmail.com>
2026-04-23 16:22:45 +0200

    fix public route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -76,7 +76,7 @@ return res.status(404).json({
 });
 
 // legacy public route
-app.get("/api/public/pages/:siteId/:slug", getPublicPage);
+//app.get("/api/public/pages/:siteId/:slug", getPublicPage);
 app.use("/api/public", publicRoutes);
 
 // ========================
