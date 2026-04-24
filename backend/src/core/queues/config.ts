32ccad73e6a25ff8678db00c981c42b155a8ef99
Chaima Kaboudi <kaboudichaima123@gmail.com>
2026-04-24 22:12:44 +0200

    fix public route

--- backend/src/core/queues/config.ts
+++ backend/src/core/queues/config.ts
@@ -1,5 +1,10 @@
+// 📂 src/core/queues/config.ts
+
 export const REDIS_CONFIG = {
-  host: '192.168.183.59', // حط الـ IP متاع الـ Ubuntu بيدك هنا
-  port: 6379,
-  maxRetriesPerRequest: null
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT) || 6379,
+  password: process.env.REDIS_PASSWORD,
+  // 🔥 هذي أهم زيادة للـ Upstash
+  tls: process.env.REDIS_TLS === 'true' ? {} : undefined, 
+  maxRetriesPerRequest: null,
 };
