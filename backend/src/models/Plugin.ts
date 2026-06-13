ae46ee7c85af315275f41d367e4ff72d5ba4f39f
Chaima Kaboudi <kaboudichaima123@gmail.com>
2026-06-13 12:32:47 +0200

    fix themeContext import case

--- backend/src/models/Plugin.ts
+++ backend/src/models/Plugin.ts
@@ -1,7 +1,19 @@
-import { Table, Column, Model, DataType, HasMany } from "sequelize-typescript";
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  HasMany,
+  CreatedAt,
+} from "sequelize-typescript";
+
 import { SitePlugin } from "./SitePlugin";
 
-@Table({ tableName: "plugins", timestamps: true })
+@Table({
+  tableName: "plugins",
+  timestamps: true,
+  updatedAt: false,
+})
 export class Plugin extends Model {
   @Column({
     type: DataType.STRING,
@@ -14,13 +26,29 @@ export class Plugin extends Model {
     type: DataType.STRING,
     allowNull: false,
     unique: true,
+    field: "key",
   })
   slug!: string;
 
   @Column(DataType.TEXT)
   description?: string;
 
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: true,
+    field: "is_active",
+  })
+  isActive!: boolean;
+
+  @CreatedAt
+  @Column({
+    type: DataType.DATE,
+    field: "created_at",
+  })
+  createdAt!: Date;
+
   @HasMany(() => SitePlugin)
   siteActivations!: SitePlugin[];
 }
+
 export default Plugin;
